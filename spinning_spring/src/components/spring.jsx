5bd2702777265fc5db168f8b112cfd2b813ac5c9
TheEnquirer <huxmarv@nuevaschool.org>
2021-06-08 20:13:50 -0700

    pathing and speed control

--- spinning_spring/src/components/spring.jsx
+++ spinning_spring/src/components/spring.jsx
@@ -4,29 +4,46 @@ import Sketch from "react-p5";
 let x = 0
 let y = 0
 let theta = 0;
+let w = 0.04;
 
+let cnv;
 let size = 850;
 
 
 let path = []
 
+
 export default (props) => {
     const setup = (p5, canvasParentRef) => {
-	// use parent to render the canvas in this ref
-	// (without that p5 will render the canvas outside of your component)
-	p5.createCanvas(size, size).parent(canvasParentRef);
-    };
+	cnv = p5.createCanvas(size, size).parent(canvasParentRef)
+	//console.log(cnv)
+	//cnv.keyPressed((event) => {
+	//    console.log("Clicked on the canvas. Event:", event)
+	//})
+    }
 
     const cart = (r, theta) => {
 	return ([r * Math.cos(theta), r* Math.sin(theta)])
     };
 
+    const keyPressed = (_p5, e) => {
+	if (e.key == "ArrowUp" || e.key == "w") {
+	    console.log("up")
+	    w += 0.01
+	}
+	if (e.key == "ArrowDown" | e.key == "s") {
+	    console.log("jjd")
+	    w -= 0.01
+	}
+    }
+
     const gen = () => {
-	theta += 0.05
+	theta += w;
 	return theta
     }
 
     const draw = (p5) => {
+	//p5.clear()
 	p5.ellipseMode(p5.CENTER);
 	p5.translate(size / 2, size / 2);
 	p5.background("#231c14");
@@ -36,22 +53,24 @@ export default (props) => {
 
 	let i;
 	for (i in path) {
+	    p5.fill(`rgba(206, 201, 189, ${3/i})`);
 	    p5.ellipse(path[i][0], path[i][1], 5, 5);
 	}
+	//console.log(path.length)
 
 
-	p5.fill('rgba(0,255,0, 0.5)');
-	p5.ellipse(0, 0, size, size);
+	//p5.fill('rgba(0,255,0, 0.5)');
+	//p5.ellipse(0, 0, size, size);
 
 	p5.fill("#CEC9BD");
 
-	let x = cart(100, gen())[0]
-	let y = cart(100, gen())[1]
+	let x = cart(200, gen())[0]
+	let y = cart(200, gen())[1]
 	p5.ellipse(x, y, 25, 25);
 
 
 	path.unshift([x, y])
-	path.slice(0, 5)
+	path = path.slice(0, 100)
 	//console.log(path)
 	//console.log(cart(5, 5))
 	// NOTE: Do not use setState in the draw function or in functions that are executed
@@ -60,5 +79,5 @@ export default (props) => {
 	//x++;
 	};
 
-    return <Sketch setup={setup} draw={draw} />;
+    return <Sketch setup={setup} draw={draw} keyPressed={keyPressed}/>;
 };
